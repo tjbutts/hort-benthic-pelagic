9f5c53b0eae9b365a3b5356602570f87d531ac79
Tyler <tyler.james.butts@gmail.com>
2022-01-14 15:17:30 -0600

    Start of Hort Farm Ecosystem Resilence Project

---
+++ Step1_Dataset Cleaning.R
@@ -0,0 +1,16 @@
+## Hort Farm Ecosystem Resilience Project ###
+# Code originally written by TJ Butts January 2022
+
+#============================================#
+# STEP 1: LOAD IN DATASETS 
+#============================================#
+rm(list=ls())
+graphics.off()
+
+# Required Libraries for analysis and visualization
+if (!require(tidyverse)) install.packages('tidyverse')
+library(tidyverse)
+if (!require(magrittr)) install.packages('magrittr')
+library(magrittr)
+if (!require(lubridate)) install.packages('lubridate')
+library(lubridate) 
