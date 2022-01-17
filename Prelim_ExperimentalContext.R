d0ec30b5d7958f99be42d30c876dda90ecdcf164
Grace Wilkinson <grace.wilkinson23@gmail.com>
2022-01-17 11:07:00 -0600

    Creation of script for preliminary data exploration related to establishing the experimental context

---
+++ Prelim_ExperimentalContext.R
@@ -0,0 +1,8 @@
+## Hort Farm Ecosystem Resilience Project ###
+# Code originally written by GM Wilkinson January 2022
+
+#Experimental Context =================================
+# Exploratory time series of: 
+  # Nutrient concentrations
+  # Nutrient ratios
+  # DOC (when we get the data)
