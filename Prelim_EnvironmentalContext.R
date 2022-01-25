3cd5adf3f950a2361f87286e6e337d4157b7f94c
Tyler <tyler.james.butts@gmail.com>
2022-01-25 16:15:04 -0600

    Edited zooplantkon biomass time series to clean up and make more visually pleasing/update with most recent data

--- Prelim_EnvironmentalContext.R
+++ Prelim_EnvironmentalContext.R
@@ -61,7 +61,7 @@ stripchart(x, pch=16, cex=1.5,
            xlim= c(50,500),
            las=2)
 
-# Zooplankton biomass time series # 
+# Zooplankton biomass time series #=========================
 zoop = read_csv('hort_zp_clean_11622.csv')
 zoop
 
@@ -85,8 +85,10 @@ ye = zoop_tot[zoop_tot$pond_id=='E', 'tot_biomass']
 xf = zoop_tot[zoop_tot$pond_id=='F', 'doy']
 yf = zoop_tot[zoop_tot$pond_id=='F', 'tot_biomass']
 
-
-plot(ya~xa, type='o', col=int_col, lwd=4,cex=2,cex.axis=1.5, ylim=c(0,900), xlim=c(140,245))
+par(mai=c(0.9,1.2,0.6,0.6))
+plot(ya~xa, type='o', col=int_col, lwd=4,cex=2,cex.axis=1.5, ylim=c(0,600), xlim=c(140,245), ylab='',xlab='')
+mtext(side=2, 'zooplankton biomass', line=3, cex=2)
+mtext(side=1, 'Day of Year, 2020', line=3, cex=2)
 lines(yd~xd, type='o', col=int_col_trans, lwd=4)
 lines(yb~xb, type='o', col=low_col, lwd=4)
 lines(yf~xf, type='o', col =low_col_trans, lwd=4)
@@ -94,4 +96,29 @@ lines(yc~xc, type='o', col = high_col, lwd=4)
 lines(ye~xe, type='o', col = high_col_trans, lwd=4)
 legend('topright', legend=c('low', 'int', 'high'), col=c(low_col, int_col, high_col), pch=20)
 
-# Macroinvertebrate density time series 
+# Log scale # 
+par(mai=c(0.9,1.2,0.6,0.6))
+plot(log(ya)~xa, type='l', col=int_col, lwd=4,cex=2,cex.axis=1.5, 
+     ylim=c(log(0.1),log(600)), xlim=c(140,245), ylab='',xlab='', yaxt='n')
+mtext(side=2, 'zooplankton biomass', line=3, cex=2)
+mtext(side=1, 'Day of Year, 2020', line=3, cex=2)
+lines(log(yd)~xd, type='o', col=int_col_trans, lwd=4)
+lines(log(yb)~xb, type='o', col=low_col, lwd=4)
+lines(log(yf)~xf, type='o', col =low_col_trans, lwd=4)
+lines(log(yc)~xc, type='o', col = high_col, lwd=4)
+lines(log(ye)~xe, type='o', col = high_col_trans, lwd=4)
+legend('topright', legend=c('low', 'int', 'high'), col=c(low_col, int_col, high_col), pch=20)
+axis(side=2,
+     at=c(log(0.1), log(0.2), log(0.3), log(0.4), log(0.5), log(0.6), log(0.7), log(0.8), 
+          log(0.9), log(1),
+          log(2),log(3),log(4),log(5),log(6),log(7),log(8),log(9),log(10),
+          log(20),log(30),log(40),log(50),log(60),log(70),log(80),log(90),log(100),
+          log(200), log(300), log(400), log(500), log(600), log(700), log(800), log(900), log(1000)), #Where the tick marks should be drawn
+     labels = c('0.1','','','','','','','','','1', '', '','','','','','','','10','','','','','','','','','100', '', '', '','','','','','','1000'), las=2, cex.axis=0.5)
+
+
+# Macroinvertebrate density time series #================= 
+
+
+# Gastric Lavage Diet Fraction #=================
+
